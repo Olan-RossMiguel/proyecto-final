4897f61c8bb8b65d63ffdbe2c15fb2866399aaa1
J0n4s4n <jonh.reyes.go@gmail.com>
2025-09-26 02:35:34 -0600

    feat: :construction: Creando componente **InfoPerfil.jsx**
    Se creo este componente para modularizar la pagina profile.

---
+++ frontend/src/components/InfoPerfil.jsx
@@ -0,0 +1,28 @@
+import { HatGlasses } from 'lucide-react'
+
+/**
+ * Componente para mostrar la información del perfil de un usuario.
+ * Esta informacion es proveida por el componente padre via props,
+ * el componente padre hace la peticion al servidor para traer los datos de usuario.
+ * TODO: veo mas mantenible migrar el componente por separado y que este haga el consumo por separado.
+ *
+ * @param {object} props.user - El objeto de usuario que contiene la información a mostrar.
+ * @param {string} props.user.createdAt - La fecha de creación del usuario en formato ISO.
+ * @param {string} props.user.name - El nombre del usuario.
+ * @param {string} props.user.email - El correo electrónico del usuario.
+ * @returns {JSX.Element} La tarjeta con la información del perfil del usuario.
+ */
+export const InfoPerfil = ({ user }) => {
+  // Convierte la fecha de formato ISO a un objeto Date para poder formatearla.
+  const fechaIso = user.createdAt // '2023-10-27T10:00:00.000Z'
+  const fecha = new Date(fechaIso)
+
+  return (
+    <div className='w-full px-10 py-10 bg-black/10 rounded-xl border border-slate-800 backdrop-blur-md flex flex-col justify-center items-center'>
+      <HatGlasses size={90} strokeWidth={1} />
+      <h1 className='text-3xl'>{user.name}</h1>
+      <h2 className='italic text-gray-600 pt-3'><span className='text-emerald-600 font-bold'>#Flicker</span> <span className='text-gray-300 font-normal'>desde:</span> {fecha.toLocaleString()}</h2>
+      <p className='text-gray-300'>Direccion de correo: {user.email}</p>
+    </div>
+  )
+}
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,32 +1,6 @@
-import { HatGlasses, Loader } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
-
-/**
- * Componente para mostrar la información del perfil de un usuario.
- * Esta informacion es proveida por el componente padre via props,
- * el componente padre hace la peticion al servidor para traer los datos de usuario.
- * TODO: veo mas mantenible migrar el componente por separado y que este haga el consumo por separado.
- *
- * @param {object} props.user - El objeto de usuario que contiene la información a mostrar.
- * @param {string} props.user.createdAt - La fecha de creación del usuario en formato ISO.
- * @param {string} props.user.name - El nombre del usuario.
- * @param {string} props.user.email - El correo electrónico del usuario.
- * @returns {JSX.Element} La tarjeta con la información del perfil del usuario.
- */
-const InfoPerfil = ({ user }) => {
-  // Convierte la fecha de formato ISO a un objeto Date para poder formatearla.
-  const fechaIso = user.createdAt // '2023-10-27T10:00:00.000Z'
-  const fecha = new Date(fechaIso)
-
-  return (
-    <div className='w-full px-10 py-10 bg-black/10 rounded-xl border border-slate-800 backdrop-blur-md flex flex-col justify-center items-center'>
-      <HatGlasses size={90} strokeWidth={1} />
-      <h1 className='text-3xl'>{user.name}</h1>
-      <h2 className='italic text-gray-600 pt-3'><span className='text-emerald-600 font-bold'>#Flicker</span> <span className='text-gray-300 font-normal'>desde:</span> {fecha.toLocaleString()}</h2>
-      <p className='text-gray-300'>Direccion de correo: {user.email}</p>
-    </div>
-  )
-}
+import { Loader } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { InfoPerfil } from '../components/InfoPerfil'
 
 /**
  * Página de perfil de usuario.
